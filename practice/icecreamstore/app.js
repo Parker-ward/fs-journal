d168866f9460e219a59572fecf7cbe518d9ad09f
Parker <ward.parker26@gmail.com>
2023-01-25 16:30:01 -0700

    im getting there

---
+++ practice/icecreamstore/app.js
@@ -0,0 +1,47 @@
+let iceCreams = [{
+    name: 'Cookie Dough',
+    image: 'https://celebratingsweets.com/wp-content/uploads/2014/04/Cookie-Dough-Ice-Cream-1-5.jpg',
+    price: 1
+}, {
+    name: 'Vanilla',
+    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ultimate-vanilla-ice-cream-1628511695.jpg',
+    price: 1
+}, {
+    name: 'Strawberry',
+    image: 'https://www.realfoodwithjessica.com/wp-content/uploads/2017/07/paleostrawberryicecream2.jpg',
+    price: 2
+}]
+
+let cart = []
+
+function addItemToCart(icecreamName){
+    let foundIcecream = iceCreams.find(iceCream => iceCream.name == icecreamName)
+    console.log(foundIcecream);
+    cart.push(foundIcecream)
+
+// TODO
+drawCart()
+
+}
+
+function drawCart(){
+    let cartElem = document.getElementById('cart')
+    let template = ''
+
+    cart.forEach(item => {
+        template += `<div>${item.name} $${item.price}</div>`
+    })
+    cartElem.innerHTML = template
+
+}
+
+
+
+
+
+function getTotal(){}
+
+function checkOut(){}
+
+
+
